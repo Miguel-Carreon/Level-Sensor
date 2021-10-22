2b436d09dbad0ebbb84dc06b7c27368f2da7e507
Miguel-Carreon <a01566368@itesm.mx>
2021-10-22 16:33:48 -0600

    ESP8266 board files added

---
+++ level_prototype_WiFi_ESP8266/level_prototype_WiFi_ESP8266.ino
@@ -0,0 +1,14 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
