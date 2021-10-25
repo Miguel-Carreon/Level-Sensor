831f13f08bc24a9c5aafc9885fab9a97a416dd71
Miguel-Carreon <a01566368@itesm.mx>
2021-10-24 20:10:32 -0600

    separated code in tabs

---
+++ level_prototype_WiFi_ESP8266/Credenciales.h
@@ -0,0 +1,2 @@
+#define SECRET_SSID "Inserta dentro de las comillas tu SSID"
+#define SECRET_PASS "Inserta dentro de las comillas tu contraseña de WiFi"
--- level_prototype_WiFi_ESP8266/level_prototype_WiFi_ESP8266.ino
+++ level_prototype_WiFi_ESP8266/level_prototype_WiFi_ESP8266.ino
@@ -2,13 +2,54 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+#include "Credenciales.h"
+#include "HTML.h"
+
+// Variables Globales //
+
+
+// Crear el objeto de la clase ESP8266WebServer //
+ESP8266WebServer server(80); //El puerto 80 es el estandar para HTTP
 
 void setup() {
-  // put your setup code here, to run once:
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
 
+  Serial.println("\nESP8266 Web Server");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(NETWORK_SSID, NETWORK_PASS);
+
+  Serial.print("Connecting");
+
+  while(WiFi.status() != WL_CONNECTED){
+    delay(250);
+    Serial.print(".");
+  }
+  
+  Serial.println("\nWiFi ready");
+  Serial.print("ESP82666 IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("HTTP Server started");
+  
+}
+
+// Función para manejar las respuestas entrantes //
+void handleRoot(){
+  digitalWrite(LED_BUILTIN, HIGH);
+  server.send(200, "text/html", html_file);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  server.handleClient(); // Administrar las peticiones
+  
+  int power = WiFi.RSSI();
+  Serial.printf("Signal dB %d\n", power);
+  delay(1000);
+    
 }
